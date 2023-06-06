c23ea7fe92cdd7b8e0f65170699dd3b7f3fb6ba7
Tigo Middelkoop <tigo.middelkoop@gmail.com>
2023-06-07 00:38:12 +0200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      {/*<body className={inter.className}>{children}</body>*/}
+      <body className="h-screen w-screen">{children}</body>
     </html>
   )
 }
